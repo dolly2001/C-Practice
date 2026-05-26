38dee0eda6f7dbc600939114803f11855607e57b
dolly2001 <piyushsolanki18dec@gmail.com>
2026-05-26 16:14:09 +0530

    second commit

---
+++ array.c
@@ -0,0 +1,12 @@
+// Write a simple program to demonstrate the use of arrays in C ?
+
+#include <stdio.h>
+int main(){
+
+    int arr[] = {1, 2, 3, 4,};
+    for(int i = 0; i < 4; i++){
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
